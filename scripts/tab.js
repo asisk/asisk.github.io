bae5b5c60bb2a35b6cb8f19c89663ec20fbcae24
asisk <anniesisk@gmail.com>
2023-12-29 19:33:45 -0700

    updates to ui design page to add video and minor formatting changes

--- scripts/tab.js
+++ scripts/tab.js
@@ -1,15 +1,24 @@
+const tabBtns = document.querySelectorAll(".tablinks");
+
+tabBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    tabBtns.forEach(f => f.classList.replace("active", "tablinks"));
+    e.target.classList.toggle("active");
+  });
+});
+
 function openStage(evt, stageName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
+    // tablinks = document.getElementsByClassName("tablinks");
+    // for (i = 0; i < tablinks.length; i++) {
+    //   tablinks[i].className = tablinks[i].className.replace("active", "tablinks");
+    // }
     document.getElementById(stageName).style.display = "block";
-    evt.currentTarget.className += "active";
+    evt.currentTarget.className = "active";
   }
   
   // Get the element with id="defaultOpen" and click on it
